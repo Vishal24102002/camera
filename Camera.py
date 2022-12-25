a3799573bc075d73e833d42f0fae32e910f3d934
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2022-12-25 14:50:19 +0530

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -2,6 +2,26 @@
 From tkinter import *
 Import cv2
 
+def video():
+  Cam=cv2.videocapture(0)
+  fourcc=cv2.videowriter_fourcc('XVID')
+  Out=cv2.videowriter('output',fourcc,20(fps))
+  While(True):
+    ret,frame=Cam.read()
+    cv2.imshow('frame',frame)
+    if ret==true:
+      Out.write(frame)
+  Out.release()
+  Cam.release()
+  cv2.destroyAllWindow()
+
+def capture():
+  Cap_save=Top-level
+  name=(nam+'jpg')
+  Capt=cv2.imread(name,1)
+  
+
+
 Camera=Tk()
 Camera.title("next gen")
 Camera.geometry('400x350')
@@ -9,8 +29,28 @@ Camera.minsize(400,350)
 Camera.maxsize(800,550)
 
 #frame creation
-Frame_camera=Frame()
+Frame_camera=Frame(Camera)
+Cam=cv2.videocapture(0)
+  While(True):
+    ret,frame=cam.read()
+    Cv2.imshow('frame',frame)
+Cam.release()
+Cv2.destroyAllWindow()
 
 
+
+Frame_bottom=Frame(Camera)
+# Button list
+Button_click=RoundButton(Frame_bottom,text="Cap",bg='yellow',command=capture)
+Button_rec=RoundButton(Frame_bottom,text="Rec",bg='red',command=video)
+
+Button_click.pack()
+Button_rec.pack()
+
+Frame_side=Frame(Camera)
+
+Frame_bottom.pack(side=bottom)
+Frame_side.pack(side=left)
+Frame_camera.pack()
 
 Camera.mainloop()
